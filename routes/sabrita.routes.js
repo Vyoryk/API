dcab62ad399fb46da2e6f69c1915c155260cc950
Vyoryk <vyctor.saz@gmail.com>
2022-03-22 17:20:19 -0700

    controller delete y route delete

--- routes/sabrita.routes.js
+++ routes/sabrita.routes.js
@@ -7,6 +7,6 @@ const sabritaController = require('../controllers/sabritas.controller');
 router.get('/', sabritaController.getSabritas);
 router.post('/', sabritaController.addSabrita);
 // router.put('/', );
-// router.delete('/:id', );
+router.delete('/:id', sabritaController.deleteSabrita);
 
 module.exports = router;
